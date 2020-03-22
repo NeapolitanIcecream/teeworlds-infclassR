1d49abe90a36b530fa49bdb5ac8ca06657b41d45
Neopolitan <neopolitan@mail.ustc.edu.cn>
2020-03-22 23:34:52 +0800

    fix . to ->

--- src/game/server/entities/projectile.cpp
+++ src/game/server/entities/projectile.cpp
@@ -129,7 +129,7 @@ void CProjectile::Tick()
 		}
 		else if(m_Explosive)
 		{
-			int OwnerClass = OwnerChar.GetClass();
+			int OwnerClass = OwnerChar->GetClass();
 			if (OwnerClass == PLAYERCLASS_ARCHITECT)
 			{
 				GameServer()->CreateExplosionDisk(m_Pos, float(g_Config.m_InfArchitectGrenadeRange), float(g_Config.m_InfArchitectGrenadeRange) * 1.5f, g_Config.m_InfArchitectGrenadeDamage, float(g_Config.m_InfArchitectGrenadeForce), m_Owner, m_Weapon, m_TakeDamageMode);
